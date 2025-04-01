d833458311953ba2d4181fa80a7a6564b39578e0
TanishqMSD <tanishqkulkarni18@gmail.com>
2025-04-02 01:06:12 +0530

    Deploment

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
-  plugins: [react(),tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  },
   server: {
     allowedHosts: ['17a4-103-71-19-206.ngrok-free.app']
-  },
+  }
 })
